02801569d8d0e3d845d541c1bd4263753e7e9440
Jordi Anguela <jordi.anguela@gmail.com>
2016-01-19 21:35:29 +0100

    init project

---
+++ src/PrintDate.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+/**
+ * print-date kata
+ * 
+ * @author Jordi Anguela (janguela)
+ */
+public class PrintDate {
+	public void printCurrentDate() {
+		System.out.println(new Date());
+	}
+}
---
+++ src/PrintDateTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * @author Jordi Anguela (janguela)
+ */
+public class PrintDateTest {
+
+	@Test
+	public void printDate() throws Exception {
+		PrintDate printDate = new PrintDate();
+		printDate.printCurrentDate();
+		// How can we test this function?
+	}
+}
