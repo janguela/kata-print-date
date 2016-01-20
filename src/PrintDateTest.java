4a965899051846b52a596b7fe808b682de4e66ef
Jordi Anguela <jordi.anguela@gmail.com>
2016-01-20 11:18:35 +0100

    use Mockito to verify that println was called with the correct argument

--- src/PrintDateTest.java
+++ src/PrintDateTest.java
@@ -1,8 +1,13 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
+import java.io.PrintStream;
 import java.util.Date;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 /**
  * @author Jordi Anguela (janguela)
@@ -51,4 +56,37 @@ public class PrintDateTest {
 		// 6. teardown
 		printStream.close();
 	}
+
+	@Test
+	public void printDate_prints_current_date_tested_with_mockito() throws Exception {
+		// 1. setup data - Prepare object that is being tested
+		PrintStream mockedPrintStream = mock(java.io.PrintStream.class);
+		PrintDate printDate = new PrintDate(mockedPrintStream);
+
+		// 2. setup expectations - Prepare expectations in mock that is being
+		// used by primary object.
+
+		// 3. exercise
+		printDate.printCurrentDate();
+
+		// 4. verify expectations - Verify that correct methods has been invoked
+		// in mock
+		ArgumentCaptor<Date> argument = ArgumentCaptor.forClass(Date.class);
+		Date now = new Date();
+		verify(mockedPrintStream).println(argument.capture());
+		Date printedDate = argument.getValue();
+		assertLessThanTwoSeconds(now, printedDate);
+
+		// 5. verify state
+
+		// 6. teardown
+		mockedPrintStream.close();
+	}
+
+	private void assertLessThanTwoSeconds(Date first, Date second) {
+		long nowInMillis = first.getTime() / 2000;
+		long argumentInMillis = second.getTime() / 2000;
+		assertEquals(nowInMillis, argumentInMillis);
+	}
+
 }
